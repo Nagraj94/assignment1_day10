a2ec773fee3594308afc244a63cb683ccb88df06
Ngowda94 <Nagraj94>
2022-06-04 17:02:57 +0530

    employee presenty

---
+++ src/com/bridgelabz/AddPartTimer.java
@@ -0,0 +1,19 @@
+//package com.bridgelabz;
+//
+//public class AddPartTimer {
+//
+//    public static final int FULL_DAY = 1;
+//
+//    public static final int HALF_DAY = 2;
+//
+//    public static void main(String[] args) {
+//
+//        int empHrs = 0;
+//
+//        if(empHrs <= 8) {
+//            System.out.println("employee is full timer");
+//        }
+//        else ()
+//
+//    }
+//}
--- src/com/bridgelabz/CheckEmpPresent.java
+++ src/com/bridgelabz/CheckEmpPresent.java
@@ -1,29 +1,18 @@
 package com.bridgelabz;
 
-import java.util.Random;
 
 public class CheckEmpPresent {
 
-    public static final int FULL_DAY = 1;
-    public static final int HALF_DAY = 2;
-    public static final int ABSENT = 0;
-
     public static void main(String[] args) {
+            
+        int FULL_TIME = 1;
 
-        Random random = new Random();
-        int check;
-
-        check = random.nextInt(3);
-        switch (check){
-            case FULL_DAY:
-                System.out.println("Employee is present for full day");
-                break;
-            case HALF_DAY:
-                System.out.println("Employee is present for half day");
-                break;
-            case ABSENT:
-                System.out.println("Employee is absent today");
-            default:
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if(empCheck == FULL_TIME){
+            System.out.println("employee is present");
+        }
+        else{
+            System.out.println("employee is absent");
         }
 
     }
