a2cc90569597dce93d15f4bf7f883d9ce1f78b43
Ngowda94 <Nagraj94>
2022-06-04 17:27:45 +0530

    adding part timer

--- src/com/bridgelabz/AddPartTimer.java
+++ src/com/bridgelabz/AddPartTimer.java
@@ -1,19 +1,36 @@
-//package com.bridgelabz;
-//
-//public class AddPartTimer {
-//
-//    public static final int FULL_DAY = 1;
-//
-//    public static final int HALF_DAY = 2;
-//
-//    public static void main(String[] args) {
-//
-//        int empHrs = 0;
-//
-//        if(empHrs <= 8) {
-//            System.out.println("employee is full timer");
-//        }
-//        else ()
-//
-//    }
-//}
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class AddPartTimer {
+
+    public static void main(String[] args) {
+
+         int FULL_DAY = 1;
+         int HALF_DAY = 2;
+
+         int empHrs = 0;
+
+        Random run = new Random();
+        int check = run.nextInt(3);
+
+        if (check == FULL_DAY)
+        {
+            empHrs = 8;
+            System.out.println("full timer");
+        }
+        else if (check == HALF_DAY)
+        {
+            empHrs = 4;
+            System.out.println("part timer");
+        }
+        else
+        {
+            System.out.println("absent");
+        }
+
+        double wages = empHrs * 20;
+        System.out.println("total employee wages is: " + wages);
+
+    }
+}
